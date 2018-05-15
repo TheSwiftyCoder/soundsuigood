492e7491ee3a361456c9b9f739639c19532a6451
Jay Cohen <jesse@theswiftycoder.com>
2018-05-15 09:40:07 +0100

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 1000);
+});
